77521cc29f96634774204b9fbf511175633a30d9
DaarcyDev <Liam_alvarez@hotmail.com>
2023-03-01 14:10:13 -0600

    ProyectoPLF v3

--- file.py
+++ file.py
@@ -3,7 +3,15 @@ import tkinter.messagebox
 from tkinter.font import Font
 import customtkinter
 from PIL import Image, ImageTk
+from openpyxl import load_workbook
+import random
 
+# Cargar el archivo Excel
+workbook = load_workbook(filename='Boris.xlsx')
+sheet = workbook.active
+
+workbook2 = load_workbook(filename='Mestros.xlsx')
+sheet2 = workbook2.active
 
 # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_appearance_mode("System")
@@ -20,7 +28,7 @@ window.title("Proyecto Tkinter")
 
 
 class Actions:
-    btn_font = customtkinter.CTkFont(family="Roboto Cn", size=25) 
+    btn_font = customtkinter.CTkFont(family="Roboto Cn", size=25)
 
     indexFramePic = customtkinter.CTkFrame(window)
 
@@ -29,14 +37,15 @@ class Actions:
 
     aboutFrame = customtkinter.CTkFrame(window)
     aboutFrame2 = customtkinter.CTkFrame(window)
-    
 
     aboutFrame3 = customtkinter.CTkFrame(window)
 
     menuFrame = customtkinter.CTkFrame(window)
 
     # productsBox = customtkinter.Treeview(window,height=19, columns=("#0","#1"), selectmode="extended")
-    productsBox = ttk.Treeview(window,height=19, columns=("#0","#1"), selectmode="extended")
+    productsBox = ttk.Treeview(window, height=35, columns=(
+        "#0", "#1"), selectmode="extended")
+    
 
 
     def destroy():
@@ -56,18 +65,51 @@ class Actions:
             widget.destroy()
         Actions.productsBox.delete(*Actions.productsBox.get_children())
 
-
     def Generar():
         Actions.destroy()
 
-        
+        Actions.productsBox.pack(pady=40, padx=60, anchor="center")
+
+
         Actions.aboutFrame3.pack(
             pady=20, padx=60, expand=False, anchor="center")
+        
+        Actions.productsBox['columns'] = (
+            "Maestros","Materias","Check")
+        
+        Actions.productsBox.column("#0", width=0)
+        Actions.productsBox.column("Maestros", width=350)
+        Actions.productsBox.column("Materias", width=450)
+        Actions.productsBox.column("Check", width=150)
 
+
+        Actions.productsBox.heading("#0", text="")
+        Actions.productsBox.heading("Maestros", text="Maestros")
+        Actions.productsBox.heading("Materias", text="Materias")
+        Actions.productsBox.heading("Check", text="Chek")
+
+        # for row1 in sheet2.iter_rows(min_row=2, values_only=True):
+        #     print(row1)
+
+
+        for row1 in sheet2.iter_rows(min_row=2, values_only=True):
+            # obtener todos los datos de la segunda hoja y almacenarlos en una lista
+            row2_list = list(sheet.iter_rows(min_row=2, values_only=True))
+            # seleccionar aleatoriamente 5 elementos de la lista row2_list
+            random_row2 = random.sample(row2_list, 5)
+            # iterar sobre los valores aleatorios seleccionados
+            for val in random_row2:
+                # concatenar el valor de la primera fila con el valor aleatorio seleccionado de la segunda fila
+                # print("('"+row1[0]+"'"+","+"'"+val[0]+"')")
+                # value = row1[0]+","+val[0]
+                Actions.productsBox.insert(
+                "", customtkinter.END, text="", values=(row1[0],val[0],"1"))
+
+                
 
 
         aboutButton2 = customtkinter.CTkButton(
-            Actions.aboutFrame3, width=300, height=50,command=lambda: Actions.menu(), text="regresar",font=Actions.btn_font)
+            Actions.aboutFrame3, width=300, height=50, command=lambda: Actions.menu(), text="regresar", font=Actions.btn_font)
         aboutButton2.pack(pady=10, padx=10, side="left")
 
         Actions.indexFrame.pack_forget()
@@ -76,60 +118,52 @@ class Actions:
         Actions.aboutFrame2.pack_forget()
         Actions.indexFramePic.pack_forget()
 
-
     def Horarios():
         Actions.destroy()
 
-                
-        Actions.productsBox.pack(pady=40, padx=60)
+        Actions.indexFrame2.pack(
+            pady=20, padx=60, expand=True, anchor="center")
+
+        Actions.productsBox.pack(pady=40, padx=60, anchor="center")
 
         Actions.aboutFrame3.pack(
             pady=20, padx=60, expand=False, anchor="center")
 
-        Actions.productsBox['columns'] = ('Maestros', 'Creditos', 'Grupos','Lunes','Martes','Miercoles','Jueves','Viernes')
+        aboutLabel1 = customtkinter.CTkLabel(
+            master=Actions.indexFrame2, justify=customtkinter.LEFT, text="Horarios", font=Actions.btn_font)
+        aboutLabel1.pack(pady=10, padx=10)
+
+        Actions.productsBox['columns'] = (
+            'Materias', 'Creditos', 'Grupos', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes')
 
         Actions.productsBox.column("#0", width=0)
-        Actions.productsBox.column("Maestros", width=100)
-        Actions.productsBox.column("Creditos",width=150)
-        Actions.productsBox.column("Grupos",width=150)
-        Actions.productsBox.column("Lunes",width=150)
-        Actions.productsBox.column("Martes",width=150)
-        Actions.productsBox.column("Miercoles",width=150)
-        Actions.productsBox.column("Jueves",width=150)
-        Actions.productsBox.column("Viernes",width=150)
+        Actions.productsBox.column("Materias", width=250)
+        Actions.productsBox.column("Creditos", width=150)
+        Actions.productsBox.column("Grupos", width=150)
+        Actions.productsBox.column("Lunes", width=150)
+        Actions.productsBox.column("Martes", width=150)
+        Actions.productsBox.column("Miercoles", width=150)
+        Actions.productsBox.column("Jueves", width=150)
+        Actions.productsBox.column("Viernes", width=150)
         # Actions.productsBox.column("player_states",width=80)
         # Actions.productsBox.column("player_city",width=80)
 
-        Actions.productsBox.heading("#0",text="")
-        Actions.productsBox.heading("Maestros",text="Maestros")
-        Actions.productsBox.heading("Creditos",text="Creditos")
-        Actions.productsBox.heading("Grupos",text="Grupos")
-        Actions.productsBox.heading("Lunes",text="Lunes")
-        Actions.productsBox.heading("Martes",text="Martes")
-        Actions.productsBox.heading("Miercoles",text="Miercoes")
-        Actions.productsBox.heading("Jueves",text="Jueves")
-        Actions.productsBox.heading("Viernes",text="Viernes")
+        Actions.productsBox.heading("#0", text="")
+        Actions.productsBox.heading("Materias", text="Materias")
+        Actions.productsBox.heading("Creditos", text="Creditos")
+        Actions.productsBox.heading("Grupos", text="Grupos")
+        Actions.productsBox.heading("Lunes", text="Lunes")
+        Actions.productsBox.heading("Martes", text="Martes")
+        Actions.productsBox.heading("Miercoles", text="Miercoes")
+        Actions.productsBox.heading("Jueves", text="Jueves")
+        Actions.productsBox.heading("Viernes", text="Viernes")
 
-        # Actions.productsBox.heading("player_states",text="States")
-        # Actions.productsBox.heading("player_city",text="States")
-
-        # Actions.productsBox.insert(parent='',index='end',iid=0,text='',
-        # values=('asd','Ninja','101'))
-        # Actions.productsBox.insert(parent='',index='end',iid=1,text='',
-        # values=('asd','Ranger','102'))
-        # Actions.productsBox.insert(parent='',index='end',iid=2,text='',
-        # values=('asd','Deamon','103'))
-        # Actions.productsBox.insert(parent='',index='end',iid=3,text='',
-        # values=('asd','Dragon','104'))
-        # Actions.productsBox.insert(parent='',index='end',iid=4,text='',
-        # values=('asd','CrissCross','105'))
-        # Actions.productsBox.insert(parent='',index='end',iid=5,text='',
-        # values=('asd','ZaqueriBlack','106'))
-
-        # Actions.productsBox.pack()
+        for row in sheet.iter_rows(min_row=2, values_only=True):
+            Actions.productsBox.insert(
+                "", customtkinter.END, text="", values=tuple(row))
 
         aboutButton2 = customtkinter.CTkButton(
-            Actions.aboutFrame3, width=300, height=50,command=lambda: Actions.menu(), text="regresar",font=Actions.btn_font)
+            Actions.aboutFrame3, width=300, height=50, command=lambda: Actions.menu(), text="regresar", font=Actions.btn_font)
         aboutButton2.pack(pady=10, padx=10, side="left")
 
         Actions.indexFrame.pack_forget()
@@ -141,7 +175,6 @@ class Actions:
     def Maestros():
         Actions.destroy()
 
-        
         Actions.productsBox.pack(pady=40, padx=60)
 
         Actions.aboutFrame3.pack(
@@ -150,37 +183,23 @@ class Actions:
         Actions.productsBox['columns'] = ('maestros', 'HPlazas', 'HAsignadas')
 
         Actions.productsBox.column("#0", width=0)
-        Actions.productsBox.column("maestros", width=100)
-        Actions.productsBox.column("HPlazas",width=150)
-        Actions.productsBox.column("HAsignadas",width=150)
-        # Actions.productsBox.column("player_states",width=80)
-        # Actions.productsBox.column("player_city",width=80)
+        Actions.productsBox.column("maestros", width=350)
+        Actions.productsBox.column("HPlazas", width=150)
+        Actions.productsBox.column("HAsignadas", width=150)
 
-        Actions.productsBox.heading("#0",text="")
-        Actions.productsBox.heading("maestros",text="Maestros")
-        Actions.productsBox.heading("HPlazas",text="Horas de plaza")
-        Actions.productsBox.heading("HAsignadas",text="Horas Asignadas")
-        # Actions.productsBox.heading("player_states",text="States")
-        # Actions.productsBox.heading("player_city",text="States")
+        Actions.productsBox.heading("#0", text="")
+        Actions.productsBox.heading("maestros", text="Maestros")
+        Actions.productsBox.heading("HPlazas", text="Horas de plaza")
+        Actions.productsBox.heading("HAsignadas", text="Horas Asignadas")
 
-        Actions.productsBox.insert(parent='',index='end',iid=0,text='',
-        values=('asd','Ninja','101'))
-        # Actions.productsBox.insert(parent='',index='end',iid=1,text='',
-        # values=('asd','Ranger','102'))
-        # Actions.productsBox.insert(parent='',index='end',iid=2,text='',
-        # values=('asd','Deamon','103'))
-        # Actions.productsBox.insert(parent='',index='end',iid=3,text='',
-        # values=('asd','Dragon','104'))
-        # Actions.productsBox.insert(parent='',index='end',iid=4,text='',
-        # values=('asd','CrissCross','105'))
-        # Actions.productsBox.insert(parent='',index='end',iid=5,text='',
-        # values=('asd','ZaqueriBlack','106'))
 
-        # Actions.productsBox.pack()
 
+        for row in sheet2.iter_rows(min_row=2, values_only=True):
+            Actions.productsBox.insert(
+                "", customtkinter.END, text="", values=tuple(row))
 
         aboutButton2 = customtkinter.CTkButton(
-            Actions.aboutFrame3, width=300, height=50,command=lambda: Actions.menu(), text="regresar",font=Actions.btn_font)
+            Actions.aboutFrame3, width=300, height=50, command=lambda: Actions.menu(), text="regresar", font=Actions.btn_font)
         aboutButton2.pack(pady=10, padx=10, side="left")
 
         Actions.indexFrame.pack_forget()
@@ -199,26 +218,25 @@ class Actions:
             pady=20, padx=60, expand=True, anchor="center")
 
         aboutButton1 = customtkinter.CTkButton(
-            Actions.aboutFrame, width=300, height=50,command=lambda: Actions.Maestros(), text="Maestros",font=Actions.btn_font)
+            Actions.aboutFrame, width=300, height=50, command=lambda: Actions.Maestros(), text="Maestros", font=Actions.btn_font)
         aboutButton1.pack(pady=10, padx=10)
 
         aboutButton2 = customtkinter.CTkButton(
-            Actions.aboutFrame, width=300, height=50,command=lambda: Actions.Horarios(), text="Horarios",font=Actions.btn_font)
+            Actions.aboutFrame, width=300, height=50, command=lambda: Actions.Horarios(), text="Horarios", font=Actions.btn_font)
         aboutButton2.pack(pady=10, padx=10)
 
         aboutButton3 = customtkinter.CTkButton(
-            Actions.aboutFrame, width=300, height=50,command=lambda: Actions.Generar(), text="Generar",font=Actions.btn_font)
+            Actions.aboutFrame, width=300, height=50, command=lambda: Actions.Generar(), text="Generar", font=Actions.btn_font)
         aboutButton3.pack(pady=10, padx=10)
 
         aboutButton4 = customtkinter.CTkButton(
-            Actions.aboutFrame2, width=300, height=50,command=lambda: Actions.index(), text="regresar",font=Actions.btn_font)
+            Actions.aboutFrame2, width=300, height=50, command=lambda: Actions.index(), text="regresar", font=Actions.btn_font)
         aboutButton4.pack(pady=10, padx=10, side="left")
 
         Actions.indexFrame.pack_forget()
         Actions.indexFrame2.pack_forget()
         Actions.aboutFrame3.pack_forget()
-        
-        
+
         Actions.indexFramePic.pack_forget()
         Actions.productsBox.pack_forget()
 
@@ -229,21 +247,53 @@ class Actions:
         Actions.aboutFrame2.pack(
             pady=20, padx=60, expand=True, anchor="center")
 
+        imagen = Image.open("about.jpg")
+        nuevo_tamano = (500, 800)
+        imagen = imagen.resize(nuevo_tamano)
+        imagen_tk = ImageTk.PhotoImage(imagen)
+
+        label = customtkinter.CTkLabel(
+            Actions.aboutFrame, image=imagen_tk, text="")
+        label.pack(side="left")
 
         aboutLabel1 = customtkinter.CTkLabel(
-            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="Alvarez Peralta Liam",font=Actions.btn_font)
+            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="Nombre:", font=Actions.btn_font)
         aboutLabel1.pack(pady=10, padx=10)
 
         aboutLabel2 = customtkinter.CTkLabel(
-            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="5 Julio 2001",font=Actions.btn_font)
+            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="Liam Alvarez Peralta", font=Actions.btn_font)
         aboutLabel2.pack(pady=10, padx=10,)
 
         aboutLabel3 = customtkinter.CTkLabel(
-            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="Cuernavaca Morelos Mexico",font=Actions.btn_font)
+            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="Lugar:", font=Actions.btn_font)
         aboutLabel3.pack(pady=10, padx=10,)
 
+        aboutLabel4 = customtkinter.CTkLabel(
+            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="Zacatepec, Morelos", font=Actions.btn_font)
+        aboutLabel4.pack(pady=10, padx=10,)
+
+        aboutLabel5 = customtkinter.CTkLabel(
+            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="Fecha:", font=Actions.btn_font)
+        aboutLabel5.pack(pady=10, padx=10,)
+
+        aboutLabel6 = customtkinter.CTkLabel(
+            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="28-Febrero-2023", font=Actions.btn_font)
+        aboutLabel6.pack(pady=10, padx=10,)
+
+        aboutLabel7 = customtkinter.CTkLabel(
+            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="Instituto Tecnologico de Zacatepec", font=Actions.btn_font)
+        aboutLabel7.pack(pady=10, padx=10,)
+
+        aboutLabel8 = customtkinter.CTkLabel(
+            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="Proyecto:", font=Actions.btn_font)
+        aboutLabel8.pack(pady=10, padx=10,)
+
+        aboutLabel9 = customtkinter.CTkLabel(
+            master=Actions.aboutFrame, justify=customtkinter.LEFT, text="Asignacion de maestros a grupos usando PLR", font=Actions.btn_font)
+        aboutLabel9.pack(pady=10, padx=10,)
+
         aboutButton2 = customtkinter.CTkButton(
-            Actions.aboutFrame2, width=300, height=50,command=lambda: Actions.index(), text="regresar",font=Actions.btn_font)
+            Actions.aboutFrame2, width=300, height=50, command=lambda: Actions.index(), text="regresar", font=Actions.btn_font)
         aboutButton2.pack(pady=10, padx=10, side="left")
 
         Actions.indexFrame.pack_forget()
@@ -263,7 +313,6 @@ class Actions:
         Actions.indexFramePic.pack(
             pady=20, padx=60, expand=False, anchor="w")
 
-
         imagen = Image.open("itz.png")
         nuevo_tamano = (300, 300)
         imagen = imagen.resize(nuevo_tamano)
@@ -271,7 +320,7 @@ class Actions:
 
         label = customtkinter.CTkLabel(
             Actions.indexFramePic, image=imagen_tk, text="")
-        label.pack(pady=10, padx=10,side="left")
+        label.pack(pady=10, padx=10, side="left")
 
         imagen = Image.open("tnm.jpg")
         nuevo_tamano = (300, 300)
@@ -280,37 +329,43 @@ class Actions:
 
         label = customtkinter.CTkLabel(
             Actions.indexFramePic, image=imagen_tk, text="")
-        label.pack(pady=10, padx=10,side="right")
-        
+        label.pack(pady=10, padx=10, side="right")
+
+        imagen = Image.open("isc.jpg")
+        nuevo_tamano = (300, 300)
+        imagen = imagen.resize(nuevo_tamano)
+        imagen_tk = ImageTk.PhotoImage(imagen)
+
+        label = customtkinter.CTkLabel(
+            Actions.indexFramePic, image=imagen_tk, text="")
+        label.pack(pady=10, padx=10, side="left")
 
         indexLabel1 = customtkinter.CTkLabel(
-            master=Actions.indexFrame, justify=customtkinter.LEFT, text="Tecnologico Nacional de Mexico",font=Actions.btn_font)
+            master=Actions.indexFrame, justify=customtkinter.LEFT, text="Tecnologico Nacional de Mexico", font=Actions.btn_font)
         indexLabel1.pack(pady=100, padx=10)
 
         indexLabel2 = customtkinter.CTkLabel(
-            master=Actions.indexFrame, justify=customtkinter.LEFT, text="Instituto Tecnologico de Zacatepec",font=Actions.btn_font)
+            master=Actions.indexFrame, justify=customtkinter.LEFT, text="Instituto Tecnologico de Zacatepec", font=Actions.btn_font)
         indexLabel2.pack(pady=30, padx=10)
 
         indexLabel3 = customtkinter.CTkLabel(
-            master=Actions.indexFrame, justify=customtkinter.LEFT, text="Ingenieria en Sistemas Computacionales",font=Actions.btn_font)
+            master=Actions.indexFrame, justify=customtkinter.LEFT, text="Ingenieria en Sistemas Computacionales", font=Actions.btn_font)
         indexLabel3.pack(pady=30, padx=10)
 
         indexLabel4 = customtkinter.CTkLabel(
-            master=Actions.indexFrame, justify=customtkinter.LEFT, text="Programacion Logica y Funcional",font=Actions.btn_font)
+            master=Actions.indexFrame, justify=customtkinter.LEFT, text="Programacion Logica y Funcional", font=Actions.btn_font)
         indexLabel4.pack(pady=30, padx=10)
 
-        
-
         indexButton1 = customtkinter.CTkButton(
-            Actions.indexFrame2, width=300, height=50,command=lambda: Actions.menu(), text="Menu",font=Actions.btn_font)
+            Actions.indexFrame2, width=300, height=50, command=lambda: Actions.menu(), text="Menu", font=Actions.btn_font)
         indexButton1.pack(pady=10, padx=10, side="left")
 
         indexButton2 = customtkinter.CTkButton(
-            Actions.indexFrame2, width=300, height=50,command=lambda: Actions.about(), text="Acerca de",font=Actions.btn_font)
+            Actions.indexFrame2, width=300, height=50, command=lambda: Actions.about(), text="Acerca de", font=Actions.btn_font)
         indexButton2.pack(pady=10, padx=10, side="left")
 
         indexButton3 = customtkinter.CTkButton(
-            Actions.indexFrame2, width=300, height=50,command=lambda: Actions.exit(), text="Salir",font=Actions.btn_font)
+            Actions.indexFrame2, width=300, height=50, command=lambda: Actions.exit(), text="Salir", font=Actions.btn_font)
         indexButton3.pack(pady=10, padx=10, side="left")
 
         Actions.aboutFrame.pack_forget()
